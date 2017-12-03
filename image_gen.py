064a8d727ff487d4f69824451aa3443d7cb4aef2
jingzhuyan <jingzhuyan91@gmail.com>
2017-12-02 17:27:19 -0800

    pickled matrix in input and output

--- image_gen.py
+++ image_gen.py
@@ -3,7 +3,7 @@
 """
 Created on Sun Nov 19 21:45:22 2017
 
-@author: jingzhuyan
+@author: jili, jingzhuyan
 """
 
 import matplotlib.pyplot as plt
@@ -11,18 +11,17 @@ from matplotlib.backend_bases import RendererBase
 from scipy import signal
 from scipy.io import wavfile
 #import soundfile as sf
-import os
+import os, pickle, random
 import numpy as np
 from PIL import Image
 from scipy.fftpack import fft
 from random import shuffle
-import random
-from scipy import misc
+#from scipy import misc
 #%matplotlib inline
 
 #====set file path====
 #root_path='/Users/jili/Box Sync/speechRec/'
-root_path='/Users/jingzhuyan/Documents/GitHub'
+root_path='/Users/jingzhuyan/Documents/kaggle'
 img_path_train = root_path+'/image/train/'
 img_path_test = root_path+'/image/test/'
 img_path_validate = root_path+'/image/validate/'
@@ -33,8 +32,8 @@ tg_id = dict((subFolderList[i], i) for i in range(len(subFolderList)))
 
 #determines how many to sample from unknow label, and sample from unknown
 avg_class_count = int(np.average([len([y for y in os.listdir(img_path_train) if y.startswith(l+'_')])for l in subFolderList[:-2]]))
-all_files = [y for y in os.listdir(img_path_train) if (('.png' in y) & (~y.startswith('unknown_')))]
-all_files_unk = [y for y in os.listdir(img_path_train) if (('.png' in y)& (y.startswith('unknown_')))]
+all_files = [y for y in os.listdir(img_path_train) if (('.pkl' in y) & (~y.startswith('unknown_')))]
+all_files_unk = [y for y in os.listdir(img_path_train) if (('.pkl' in y)& (y.startswith('unknown_')))]
 all_files_unk = [ all_files_unk[i] for i in sorted(random.sample(range(len(all_files_unk)), avg_class_count)) ]
 all_files += all_files_unk
 shuffle(all_files)
@@ -47,7 +46,8 @@ def get_batch(BATCH_SIZE):
             spec_batch=np.zeros(shape=[BATCH_SIZE, 99, 81]) 
             tg_batch=np.zeros([BATCH_SIZE, NUM_CLASSES])
             for k, file in enumerate(all_files[j*BATCH_SIZE:(j+1)*BATCH_SIZE]):
-                padded_spectrogram = misc.imread(img_path_train+file,mode='I')
+                #padded_spectrogram = misc.imread(img_path_train+file,mode='I')
+                padded_spectrogram=pickle.load(open(img_path_train+file,'rb'))
                 spec_batch[k,:,:] = padded_spectrogram
                 tg=file.split('_')[0]
                 tg_batch[k, tg_id[tg]] = 1                
